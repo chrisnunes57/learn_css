2f83b8508ea2cfaf5ed3efac30f354c58149c5a2
Chris Nunes <chrisnunes57@gmail.com>
2020-06-18 00:09:19 -0500

    Added nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
+
+          <Navigation />
+
           <Switch>
             <Route path="/about">
               About
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navigation.scss";
+
+function Navigation() {
+  return (
+    <div className="nav-wrapper">
+        henlo nav!
+    </div>
+  );
+}
+
+export default Navigation;
